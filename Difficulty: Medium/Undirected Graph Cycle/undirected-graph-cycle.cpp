489f4f6d061a22cb01e4019c0ff5504d1711f1c7
Aman Kumar Singh <97456601+AmanL02@users.noreply.github.com>
2024-08-29 11:27:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -4,33 +4,59 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+  private:
+    bool dfs(int node,int parent,vector<int>& visted,vector<int> adj[]){
+        visted[node]=1;
+        for(auto it : adj[node]){
+            if(!visted[it]){
+                if(dfs(it,node,visted,adj)==true){
+                    return true;
+                }
+            }else if(it!=parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
+        
+        vector<int> visted(V,0);
         for(int i=0;i<V;i++){
-            queue<pair<int,int>>qt;
-            vector<int> visted(V,0);
-            qt.push({i,-1});
-        
-        
-        while(!qt.empty()){
-            auto it=qt.front();
-            qt.pop();
-            int x=it.first;
-            visted[x]=visted[x]+1;
-            if(visted[x]>1){
-                return 1;
-            }
-            for(int i=0;i<adj[x].size();i++){
-                
-                if(adj[x][i] == it.second)
-                    continue;
-                qt.push({adj[x][i],x});
+            if(!visted[i]){
+                if(dfs(i,-1,visted,adj)==true){
+                    return true;
+                }
             }
         }
-        }
-        return 0;
+        return false;
+        
+        
+        // for(int i=0;i<V;i++){
+        //     queue<pair<int,int>>qt;
+        //     vector<int> visted(V,0);
+        //     qt.push({i,-1});
+        //     while(!qt.empty()){
+        //         auto it=qt.front();
+        //         qt.pop();
+        //         int x=it.first;
+        //         visted[x]=visted[x]+1;
+        //         if(visted[x]>1){
+        //             return 1;
+        //         }
+        //         for(int i=0;i<adj[x].size();i++){
+                    
+        //             if(adj[x][i] == it.second)
+        //                 continue;
+        //             qt.push({adj[x][i],x});
+        //         }
+        //     }
+        // }
+        // return 0;
     }
 };
 
